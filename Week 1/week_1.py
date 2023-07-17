4af72b91780fd8655c7ff06295f89303e6ad808e
itsTofan <106501521+itsTofan@users.noreply.github.com>
2023-07-17 15:12:23 +0930

    Troubleshooting & Debugging

---
+++ Week 1/week_1.py
@@ -0,0 +1,28 @@
+"""
+Troubleshooting & Debugging
+
+1. Troubleshooting is the process of identifying, analyzing and solving problem
+- Binary Search
+
+2. Debugging is the process of identifying, analying and removing bugs in a system
+
+3. Debuggers let us follow the code line by line, inspect changes in variable assingments,
+interrupt the program when a specific condition is met and more
+
+4. Problem Solving Steps
+- Getting Information ie documention, manual, ask 
+     Reproduction case, a clear description of how and when the problem apperars
+- Finding the root cause
+- Performing the necessary remediation
+
+5. Silently Crashing Application
+strace
+
+Tools
+tcpdump and wireshark show ongoing network connection
+ps, top, free show number of resources used in a system
+strace to look at the systamm call made by a program
+ltrace to look at the library call made by software
+
+
+"""
